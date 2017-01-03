6bb8f5c873435349c109adcbdb694f121540bf91
FRANCESCO PAOLO DELLAQUILA <paolodellaquila1999@gmail.com>
2017-01-03 23:13:55 +0100

    add day 1 progress

---
+++ Prometheus.py
@@ -0,0 +1,230 @@
+# Prometheus text editor to describe all programming days
+
+# module
+import sys
+from pip._vendor.distlib.compat import raw_input
+from datetime import datetime
+import os.path
+import json
+import os
+# ----------------------function----------------------
+
+# modify number of day in json
+def update_json_day(day):
+    # open config.json file (read)
+    with open("config.json", "r") as data_file:
+        data = json.load(data_file)
+        data["Day"] = str(day)
+
+    # open config.json file (write)
+    with open("config.json", "w") as data_file:
+        data_file.write(json.dumps(data))
+
+    # close
+    data_file.close()
+
+
+# load user data from json
+def data_json():
+    # open config.json file(read)
+    with open("config.json", "r") as data_file:
+        data = json.load(data_file)
+        # open in (a) file with progress
+        with open("log.txt", "a") as prometheus_file:
+            # Date
+            date = datetime.now()
+            day = int(data["Day"])
+            day += 1
+            prometheus_file.write(
+                "----------Day " + "[" + data["Day"] + "]" + " on Date " + "(" + str(date.day) + "/" + str(
+                    date.month) + "/" + str(date.year) + ")" + "----------" + "\n")
+            prometheus_file.write("\n")
+
+            # call update_json_day
+            update_json_day(day)
+
+            # Name and Surname
+            prometheus_file.write("Name: " + data["Name"] + "\n")
+            prometheus_file.write("\n")
+            prometheus_file.write("Surname: " + data["Surname"] + "\n")
+            prometheus_file.write("\n")
+
+        # close json
+        data_file.close()
+        #close file
+        prometheus_file.close()
+
+
+# load data from user input
+def data_user():
+    # open in (a) file with progress
+    with open("log.txt", "a") as prometheus_file:
+        # Progress
+        check_progress = False
+        while check_progress == False:
+            check_response = False
+            progress = input("Write your progress: ")
+            progress.lower()
+            while check_response == False:
+                response = input("Rewrite progress? (yes/no): ")
+                response.lower()
+                if response == "yes" or response == "no":
+                    check_response = True
+                else:
+                    print("wrong action")
+            if response == "no":
+                check_progress = True
+
+        # Thought
+        check_thought = False
+        while check_thought == False:
+            check_response = False
+            thought = input("Write your thought: ")
+            progress.lower()
+            while check_response == False:
+                response = input("Rewrite thought? (yes/no): ")
+                response.lower()
+                if response == "yes" or response == "no":
+                    check_response = True
+                else:
+                    print("wrong action")
+            if response == "no":
+                check_thought = True
+
+        # Link
+        check_link = False
+        while check_link == False:
+            check_response = False
+            link = input("Write your link: ")
+            progress.lower()
+            while check_response == False:
+                response = input("Rewrite link? (yes/no): ")
+                response.lower()
+                if response == "yes" or response == "no":
+                    check_response = True
+                else:
+                    print("wrong action")
+            if response == "no":
+                check_link = True
+
+        # write from progress to link on file
+        prometheus_file.write(
+            "Progress :" + " " + progress + "\n\n" + "Thought :" + " " + thought + "\n\n" + "Link :" + " " + link + "\n\n\n")
+
+        # tweet
+        print(
+            "https://twitter.com/intent/tweet/?text=" + " " + "Progress :" + " " + progress + " " + "Thought :" + " " + thought +
+            " " + "Link :" + " " + link + " " + " " + "100daysofcode")
+
+    print("\n\nOpen this link on browser to send tweet\n\n")
+
+    #close file
+    prometheus_file.close()
+
+def push():
+    path = os.path.abspath('Prometheus')
+    commit = str(raw_input("\n\nInsert commit: "))
+    os.system("cd/" + path)
+    os.system("git add .")
+    os.system("git commit -m" + "\"" + commit + "\"")
+    os.system("git push")
+
+def md_to_txt():
+    prevName = "log.md"
+    nextName = "log.txt"
+    os.rename(prevName,nextName)
+
+def txt_to_md():
+    prevName = "log.txt"
+    nextName = "log.md"
+    os.rename(prevName,nextName)
+
+# add all progress to file
+def add():
+
+    """format log's day:
+    |------------------------------------Day [N] on Date (current date)------------------------------------|
+    |
+    |Name and Username (user data)
+    |                                ------from json
+    |Progress (Current Progress)
+    |
+    |Thoughts (Thoughts)
+    |
+    |Links (Link to the project)
+    |
+    |                                 ------from user
+    """
+
+    #convert md file to txt
+    md_to_txt()
+
+    # data from json
+    data_json()
+
+    # data from user
+    data_user()
+
+    #convert txt file to md
+    txt_to_md()
+
+    #push on repo
+    push()
+
+    print("\nOk... all things have been saved. Now we'll push on repo...\n")
+
+
+# read all file and print
+def read_a():
+    if os.path.exists("log.md") == False:
+        print("You haven't write yet")
+    else:
+        #convert md file to txt
+        md_to_txt()
+
+        with open("log.txt", "r") as prometheus_file:
+            print(prometheus_file.read())
+
+        #convert txt file to md
+        txt_to_md()
+
+
+# read and print specific progress
+def help():
+    print ("\nadd: add progress to file and push on repo\n" + "\nread_a: read all file and print contens on screen\n")
+# menu function
+def menu(topic):
+    if topic == "add":
+        add()
+    elif topic == "read_a":
+        read_a()
+    elif topic == "help":
+        help()
+
+
+# ----------------------main----------------------
+if len(sys.argv) == 1:
+
+    print("Welcome to Prometheus: Python text editor\n")
+
+    print("You can: \n"
+          "- (Add) new thing\n"
+          "- (Read_A) all file\n"
+          "- (Help) show possible command\n\n")
+
+    check_topic = False
+
+    while check_topic == False:
+        topic = input("What do you want to do?").lower()
+        if topic == "add" or topic == "read_a" or topic == "help":
+            check_topic = True
+        else:
+            print("wrong action")
+    # call menu
+    menu(topic)
+else:
+    if sys.argv[1].lower() == "add" or sys.argv[1].lower() == "read_a" or sys.argv[1].lower() == "help":
+        # call menu
+        menu(sys.argv[1].lower())
+    else:
+        print("command not found")
--- log.md
+++ log.md
@@ -28,3 +28,16 @@
 **Link(s) to work**
 1. [Find the Longest Word in a String](https://www.freecodecamp.com/challenges/find-the-longest-word-in-a-string)
 2. [Title Case a Sentence](https://www.freecodecamp.com/challenges/title-case-a-sentence)
+----------Day [1] on Date (3/1/2017)----------
+
+Name: #
+
+Surname: #
+
+Progress : improve python skills
+
+Thought : i'm making a stable version of prometheus tools for 100daysofcode 
+
+Link : https://github.com/Francescopaolo44/Prometheus
+
+
